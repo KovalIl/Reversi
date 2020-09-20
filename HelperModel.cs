7db05ab5730f3e08ed4356b7a5a0688d2c6c2eb5
KovalI <59292713+KovalIl@users.noreply.github.com>
2020-09-20 17:22:13 +0300

    Add files via upload

---
+++ HelperModel.cs
@@ -0,0 +1,34 @@
+﻿public class HelperModel
+{
+    public int PosX;
+    public int PosZ;
+    public int ForX;
+    public int ForZ;
+    public int ResultX;
+    public int ResultZ;
+    public string NoneLayer;
+    public string CurrentLayer;
+
+    public HelperModel(int posX, int posZ, string noneLayer, string currentLayer)
+    {
+        this.PosX = posX;
+        this.PosZ = posZ;
+        this.NoneLayer = noneLayer;
+        this.CurrentLayer = currentLayer;
+    }
+
+    public int GetX()
+    {
+        return PosX + ForX;
+    }
+
+    public int GetZ()
+    {
+        return PosZ + ForZ;
+    }
+
+    public bool isBorder(int x, int z)
+    {
+        return (x * ForX > ResultX) || (z * ForZ > ResultZ);
+    }
+}
---
+++ SpawnModel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SpawnModel
+{
+    public int PosX;
+    public int PosZ;
+    public string None;
+    public string Next;
+    public string Current;
+    public bool IsBlack;
+    public GameObject Chip;
+    public GameObject ChipParent;
+    public Vector3 Position;
+
+    public SpawnModel(GameObject chipParent, Vector3 position, int posX, int posZ, string noneLayer)
+    {
+        this.Position = position;
+        this.ChipParent = chipParent;
+        this.PosX = posX;
+        this.PosZ = posZ;
+        this.None = noneLayer;
+    }
+}
